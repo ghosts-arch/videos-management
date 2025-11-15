d34743502eb746d0afc308745bedec012eed2c9e
ghosts-arch <ronan.questier@mailfence.com>
2025-11-15 02:59:50 +0100

    import env from process

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,7 @@
-import { env } from "$env/dynamic/private";
 import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/svelte";
 
 export const { signIn, useSession, signOut } = createAuthClient({
-	baseURL: env.BETTER_AUTH_URL,
+	baseURL: process.env.BETTER_AUTH_URL,
 	plugins: [adminClient()],
 });
