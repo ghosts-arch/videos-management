40275e81af474ca2d08b83b8aa5ecb7152bf3b47
ghosts-arch <ronan.questier@mailfence.com>
2025-11-15 01:58:12 +0100

    remove log

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,9 +5,6 @@ import { env } from "$env/dynamic/private";
 if (!env.TURSO_DATABASE_URL) throw new Error("DATABASE_URL is not set");
 if (!env.TURSO_AUTH_TOKEN) throw new Error("TURSO_AUTH_TOKEN is not set");
 
-// const client = new Database(env.DATABASE_URL);
-console.log(env.TURSO_DATABASE_URL);
-
 const client = createClient({
 	url: env.TURSO_DATABASE_URL,
 	authToken: env.TURSO_AUTH_TOKEN,
