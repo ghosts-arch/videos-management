ca963f162c5db81d53561fd82265e4eec0a8b281
ghosts-arch <ronan.questier@mailfence.com>
2025-11-15 02:57:48 +0100

    update auth client url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,6 @@ import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/svelte";
 
 export const { signIn, useSession, signOut } = createAuthClient({
-	baseURL: "http://localhost:5173/",
+	baseURL: env.BETTER_AUTH_URL,
 	plugins: [adminClient()],
 });
